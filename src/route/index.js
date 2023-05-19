7fbeab8a295352ac7dec2309b94e1813664984c4
AnKub <ankub3088@gmail.com>
2023-05-19 18:41:36 +0300

    6resume

--- src/route/index.js
+++ src/route/index.js
@@ -50,6 +50,7 @@ router.get('/', function (req, res) {
 router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
+    layout: 'basic',
     // ↙ сюди вводимо JSON дані
     page: {
       title: 'Resume | Summary',
@@ -74,6 +75,7 @@ router.get('/summary', function (req, res) {
 router.get('/skills', function (req, res) {
   // ↙ сюди вводимо JSON дані
   res.render('skills', {
+    layout: 'basic',
     page: {
       title: 'Resume | Skills',
     },
@@ -134,6 +136,7 @@ router.get('/skills', function (req, res) {
 router.get('/education', function (req, res) {
   // ↙ сюди вводимо JSON дані
   res.render('Education', {
+    layout: 'basic',
     page: {
       title: 'Resume | Education',
     },
@@ -175,7 +178,7 @@ router.get('/education', function (req, res) {
 router.get('/work', function (req, res) {
   // ↙ сюди вводимо JSON дані
   res.render('work', {
-    layout: 'big',
+    layout: 'basic',
     page: {
       title: 'Resume | Work',
     },
@@ -227,6 +230,137 @@ router.get('/work', function (req, res) {
     },
 
     footer,
+  })
+})
+
+router.get('/web', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('web', {
+    layout: 'basic',
+    page: {
+      title: 'Resume | Web',
+    },
+    web: {
+      languages: [
+        {
+          name: 'HTML',
+          version: 'HTML5',
+          description:
+            'Hypertext Markup Language is the standard markup language for creating web pages and web applications.',
+          elements: [
+            {
+              name: 'div',
+              description:
+                'Defines a division or a section in an HTML document.',
+              attributes: [
+                {
+                  name: 'id',
+                  description:
+                    'Specifies a unique id for an HTML element.',
+                },
+                {
+                  name: 'class',
+                  description:
+                    'Specifies one or more class names for an HTML element.',
+                },
+              ],
+            },
+            {
+              name: 'p',
+              description:
+                'Defines a paragraph in an HTML document.',
+              attributes: [
+                {
+                  name: 'id',
+                  description:
+                    'Specifies a unique id for an HTML element.',
+                },
+                {
+                  name: 'class',
+                  description:
+                    'Specifies one or more class names for an HTML element.',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: 'CSS',
+          version: 'CSS3',
+          description:
+            'Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in HTML or XML.',
+          properties: [
+            {
+              name: 'color',
+              description: 'Sets the color of the text.',
+              values: [
+                {
+                  value: 'red',
+                  description:
+                    'Sets the text color to red.',
+                },
+                {
+                  value: 'blue',
+                  description:
+                    'Sets the text color to blue.',
+                },
+              ],
+            },
+            {
+              name: 'background-color',
+              description:
+                'Sets the background color of an element.',
+              values: [
+                {
+                  value: 'white',
+                  description:
+                    'Sets the background color to white.',
+                },
+                {
+                  value: 'black',
+                  description:
+                    'Sets the background color to black.',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: 'JavaScript',
+          version: 'ES6',
+          description:
+            'JavaScript is a programming language used to create interactive effects within web browsers.',
+          functions: [
+            {
+              name: 'alert()',
+              description:
+                'Displays an alert box with a specified message and an OK button.',
+              parameters: [
+                {
+                  name: 'message',
+                  type: 'string',
+                  description:
+                    'The message to display in the alert box.',
+                },
+              ],
+            },
+            {
+              name: 'getElementById()',
+              description:
+                'Returns the element with the specified ID.',
+              parameters: [
+                {
+                  name: 'id',
+                  type: 'string',
+                  description:
+                    'The ID of the element to find.',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
   })
 })
 // Підключаємо роутер до бек-енду
